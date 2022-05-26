bbed7d3bbba46d7612226620dc5e39fc8e024e89
shazid98 <shazid.tipu@gmail.com>
2022-05-26 23:42:23 +0100

    update

--- 24052022/airplane_task/flight_trip.py
+++ 24052022/airplane_task/flight_trip.py
@@ -8,16 +8,18 @@ class Flight(Airplane):
 
     def __init__(self):
         super().__init__()
+        self.no = ""
         self.date = now
         self.origin = "London"
         self.destination = "Manchester"
-        self.passenger_list = []
+        self.passenger_list = {}
 
-    def create_flight(self, date, origin, destination):
+    def create_flight(self, no, date, origin, destination):
+        self.no = no
         self.date = date
         self.origin = origin
         self.destination = destination
-        self.flights.append([date, origin, destination])
+        self.flights[no] = [date, origin, destination]
 
     def get_flight_details(self):
         return f"Date: {self.date}, Origin: {self.origin}, Destination: {self.destination}"
--- 24052022/airplane_task/passenger.py
+++ 24052022/airplane_task/passenger.py
@@ -8,32 +8,50 @@ class Passenger(Flight):
         self.name = name
         self.surname = surname
         self.passport = passport
-        self.passenger_details = [name, surname, passport]
+        self.passenger_details = surname + " " + name
 
     def get_passport(self):
         return self.passport
 
-    def buy_flight(self, date, origin, destination):
+    def add_passenger(self, passport, passenger):
+        self.passenger_list[passport] = passenger
+
+    def buy_flight(self, no, date, origin, destination):
+        self.no = no
         self.date = date
         self.origin = origin
         self.destination = destination
         if len(self.passenger_list) > self.capacity:
-            print("Ran out of seats, no more can be sold")
+            return f"Ran out of seats, no more can be sold"
         else:
-            self.passenger_list.append(self.passenger_details)      # need to fix this
+            self.passenger_list[self.passport] = self.passenger_details      # need to fix this
 
 
 turkey = Flight()
-turkey.create_flight("24/05/2022", "London", "Istanbul")
+turkey.create_flight(1234, "24/05/2022", "London", "Istanbul")
+usa = Flight()
+usa.create_flight(1235, "26/05/2022", "London", "New York")
+
+print(usa.get_flight_details())
 print(turkey.get_flight_details())
 #
-eric = Passenger("Eric", "Smith", 6757843)
-print(eric.get_passport())
-eric.buy_flight(turkey.date, turkey.origin, turkey.destination)
-#
-print(turkey.check_passengers())
+# print(usa.check_flights())
 
+
+eric = Passenger("Eric", "Smith", 6757843)
+eric.buy_flight(turkey.no, turkey.date, turkey.origin, turkey.destination)
+eric.add_passenger(6757843, eric)
+print(usa.check_passengers())
+
+# print(eric.get_passport())
+# eric.buy_flight(turkey.date, turkey.origin, turkey.destination)
+# #
+# print(turkey.check_passengers())
+#
 # damian = Passenger("Damian", "Martin", 3948928)
+# damian.buy_flight(turkey.date, turkey.origin, turkey.destination)
+#
+# print(turkey.check_passengers())
 
 
 
